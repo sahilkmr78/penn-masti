fe9261e39e567f2a16dbe04ef4ec8c402aef8457
= <ashwinb10@gmail.com>
2016-07-08 19:22:42 -0400

    added cards to about page

--- web/src/js/components/about/index.js
+++ web/src/js/components/about/index.js
@@ -1,6 +1,7 @@
 require('./about-page.scss');
 
 import React from 'react';
+import Card from 'material-ui/Card';
 import ImageGallery from 'react-image-gallery';
 
 // IMPORTANT: make sure all images being used have the same aspect ratio
@@ -29,22 +30,20 @@ export default class AboutPage extends React.Component {
   render() {
     return (
       <div className="about-page">
-        <div className="about-page-container">
-          <div className='about-pics-gallery'>
-            <ImageGallery
-              ref={r => this._imageGallery = r}
-              items={this.imageItems}
-              autoPlay={false}
-              slideInterval={3000}
-              infinite={false}
-            />
-          </div>
+        <Card className='about-pics-gallery' zDepth={2}>
+          <ImageGallery
+            ref={r => this._imageGallery = r}
+            items={this.imageItems}
+            autoPlay={false}
+            slideInterval={3000}
+            infinite={false}
+          />
+        </Card>
 
-          <div className="about-info-para">
-            <h2>About Masti</h2>
-            <span className='about-info-para-text'>Masti means flirtation or mischief, and its members certainly have a healthy dose of each. In addition, Mastifellows love to dance. Masti has recently integrated numerous dance styles such as Bharata Natyam, Bhangra, Filmy, Lyrical, Bachata, and Hip-Hop. Penn Masti hosts an annual show in February that has historically attracted audiences of over 500 people. Masti has remained active in the South Asia Society’s culture shows, while expanding into other areas of performance as well. In 2011, Masti competed at Princeton Dance Dimensions in New Jersey and in 2012 at Naach at Hofstra University in New York. Through the dedication of its members and its unique culture, Masti hopes to make its mark as Penn’s premier co-ed South Asian fusion dance team.</span>
-          </div>
-        </div>
+        <Card className="about-info-para" zDepth={2}>
+          <div className="about-info-para-title">About Masti</div>
+          <div className='about-info-para-text'>Masti means flirtation or mischief, and its members certainly have a healthy dose of each. In addition, Mastifellows love to dance. Masti has recently integrated numerous dance styles such as Bharata Natyam, Bhangra, Filmy, Lyrical, Bachata, and Hip-Hop. Penn Masti hosts an annual show in February that has historically attracted audiences of over 500 people. Masti has remained active in the South Asia Society’s culture shows, while expanding into other areas of performance as well. In 2011, Masti competed at Princeton Dance Dimensions in New Jersey and in 2012 at Naach at Hofstra University in New York. Through the dedication of its members and its unique culture, Masti hopes to make its mark as Penn’s premier co-ed South Asian fusion dance team.</div>
+        </Card>
       </div>
     );
   }
